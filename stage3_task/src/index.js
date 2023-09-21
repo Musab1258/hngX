15ac3a9df3f210f70e0dbf5f7e21ec732f3b1ec2
Musab Habeeb <habeebmusab@gmail.com>
2023-09-21 06:38:27 +0100

    done with login

---
+++ stage3_task/src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Dashboard from './components/Dashboard';
+import ErrorPage from './components/error/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "dashboard",
+    element: <Dashboard />,
+  },
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain="https://localhost:3000/dashboard"
+      clientId="mrAWKAp2Yigvpx6DDzDgiswVTRMt1n1d"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
+
+
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
